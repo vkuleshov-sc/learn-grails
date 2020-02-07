5ca4142c003ca256eef1d166a0f03f9061a57819
vkuleshov-sc <kuleshov@scand.com>
2020-02-07 13:45:59 +0300

    Fix bug with not unique users

--- grails-app/services/testgrails/UserService.groovy
+++ grails-app/services/testgrails/UserService.groovy
@@ -21,7 +21,10 @@ class UserService {
 
     def getUserList(UserCommand params) {
         def criteria = User.createCriteria()
-        def userList = criteria.list(max: PAGE_SIZE, offset: PAGE_SIZE * (params.page - 1)) {
+        def userList = criteria.list {
+            projections {
+                groupProperty('id')
+            }
             criteriaClosure(criteria.&ilike, criteria.&ge, criteria.&le,
                 params.userNameFilter, params.dateFromFilter, params.dateToFilter)
             if (params.pokemonNameFilter) {
@@ -29,9 +32,14 @@ class UserService {
                     ilike('name', params.pokemonNameFilter)
                 }
             }
-        }
+            firstResult(params.page ? (params.page - 1) * PAGE_SIZE - 1 : 0)
+            maxResults(PAGE_SIZE)
+        }.collect { id -> User.get(id) }
         criteria = User.createCriteria()
-        def userCount = criteria.count {
+        def userCount = criteria.list {
+            projections {
+                countDistinct('id')
+            }
             criteriaClosure(criteria.&ilike, criteria.&ge, criteria.&le,
                 params.userNameFilter, params.dateFromFilter, params.dateToFilter)
             if (params.pokemonNameFilter) {
@@ -39,7 +47,7 @@ class UserService {
                     ilike('name', params.pokemonNameFilter)
                 }
             }
-        }
+        }[0]
         log.info("User total amount: ${userCount}")
         return [userList: userList.each({ user ->
             user.pokemons = user.pokemons.sort({ p1, p2 -> p1.name <=> p2.name })
